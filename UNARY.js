790d8409d1dfba77ca9d7b9d2ab8ffb3d6adf852
Sébastien Codus <49206673+sebcod@users.noreply.github.com>
2023-06-24 17:04:23 +0200

    Update UNARY.js

--- UNARY.js
+++ UNARY.js
@@ -1,15 +1,15 @@
 const MESSAGE = readline();
-let MessageBenary = "";
+let MessageBinary = "";
 let res = "";
 
 for(let i=0 ; i<MESSAGE.length ; i++){
-    MessageBenary += MESSAGE[i].charCodeAt().toString(2).padStart(7, "0");
+    MessageBinary += MESSAGE[i].charCodeAt().toString(2).padStart(7, "0");
 }
 
-for(let j=0 ; j<MessageBenary.length ; j++){
-    MessageBenary[j] === "0" && MessageBenary[j-1]  !=  "0" ? res += " 00 " : "" ;
-    MessageBenary[j] === "0" && MessageBenary[j]    === "0" ? res += "0"    : "" ;
-    MessageBenary[j] === "1" && MessageBenary[j-1]  !=  "1" ? res += " 0 "  : "" ;
-    MessageBenary[j] === "1" && MessageBenary[j]    === "1" ? res += "0"    : "" ;
+for(let j=0 ; j<MessageBinary.length ; j++){
+    MessageBinary[j] === "0" && MessageBinary[j-1]  !=  "0" ? res += " 00 " : "" ;
+    MessageBinary[j] === "0" && MessageBinary[j]    === "0" ? res += "0"    : "" ;
+    MessageBinary[j] === "1" && MessageBinary[j-1]  !=  "1" ? res += " 0 "  : "" ;
+    MessageBinary[j] === "1" && MessageBinary[j]    === "1" ? res += "0"    : "" ;
 }
 print(res.trim());
